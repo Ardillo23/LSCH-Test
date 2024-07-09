85950b60983aecc6cf55938e9239792e0a656be4
kevinclini <kevin.aguilera@clini.cl>
2024-07-08 23:54:23 -0400

    se añadio recuadro rojo de probabilidad y conexion a camara con IP webcam

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, Response, jsonify, send_file
+from flask import Flask, render_template, Response, jsonify, request
 from tensorflow.keras.models import load_model
 import numpy as np
 import cv2
@@ -18,14 +18,12 @@ auxiliar = np.concatenate((colores, preguntas), axis=None)
 auxiliar2 = np.concatenate((auxiliar, saludos), axis=None)
 actions = np.concatenate((auxiliar2, Pronombres), axis=None)
 
-# print("Número de acciones:", len(actions))
-
 sequence = []
 sentence = []
-threshold = 0.98 #  Umbral de confianza para considerar una acción reconocida. / tenia 0.8
+threshold = 0.98
 camera_active = False
 last_prediction_time = time.time()
-prediction_interval = 2  #  Variables para controlar el intervalo entre predicciones / Tenia 2
+prediction_interval = 2
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
@@ -34,28 +32,43 @@ def extract_keypoints(results):
     rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
     return np.concatenate([pose, face, lh, rh])
 
-def prob_viz(res, actions, input_frame):
+def prob_viz(res, actions, input_frame, frame_width, frame_height):
     output_frame = input_frame.copy()
     max_prob_index = np.argmax(res)
     max_prob = res[max_prob_index]
     
-    # Verificar que el índice esté dentro del rango
     if max_prob_index < len(actions):
         action = actions[max_prob_index]
     else:
         action = "Unknown"
-
-    # Draw rectangle for background (static)
-    cv2.rectangle(output_frame, (0, 0), (640, 40), (50, 50, 255), -1)
     
-    # Display only the probability on the right side of the blue rectangle
-    prob_text = f"{max_prob * 100:.2f}%"
-    text_size, _ = cv2.getTextSize(prob_text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)
-    text_x = 640 - text_size[0] - 10  # Right align with a margin of 10 pixels
-    text_y = 40 - (40 - text_size[1]) // 2  # Center vertically
-    cv2.putText(output_frame, prob_text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+    # Define la posición para el rectángulo y el texto en la parte inferior izquierda
+    rect_width = 250
+    rect_height = 40
+    start_x = 10  # Coordenada x inicial del rectángulo
+    start_y = frame_height - rect_height - 10  # Coordenada y inicial del rectángulo
+    end_x = start_x + rect_width  # Coordenada x final del rectángulo
+    end_y = frame_height - 10  # Coordenada y final del rectángulo
+    
+    # Dibuja el rectángulo
+    cv2.rectangle(output_frame, (start_x, start_y), (end_x, end_y), (50, 50, 255), -1)
+    
+    # Texto adicional "Porcentaje de probabilidad" (encima del recuadro)
+    additional_text = "Porcentaje de probabilidad"
+    additional_text_x = start_x + 10
+    additional_text_y = start_y - 10  # Posiciona este texto justo encima del rectángulo
+    cv2.putText(output_frame, additional_text, (additional_text_x, additional_text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1, cv2.LINE_AA)
+    
+    # Texto de probabilidad
+    prob_text = f"{action}: {max_prob * 100:.2f}%"
+    text_x = start_x + 10
+    text_y = start_y + rect_height - 10
+    cv2.putText(output_frame, prob_text, (text_x, text_y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 1, cv2.LINE_AA)
     
     return output_frame
+
+
+
 
 
 def mediapipe_detection(image, model):
@@ -66,16 +79,22 @@ def mediapipe_detection(image, model):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
 
-def gen():
+def gen(device_type):
     global sequence, sentence, camera_active
 
-    cap = cv2.VideoCapture(0)
+    if device_type == "mobile":
+        cap = cv2.VideoCapture('http://192.168.0.2:4747/video')  # URL del stream del teléfono
+    else:
+        cap = cv2.VideoCapture(0)  # Cámara del PC
+
     with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
         while True:
             if camera_active:
                 ret, frame = cap.read()
                 if not ret:
                     break
+                
+                frame_height, frame_width = frame.shape[:2]
 
                 image, results = mediapipe_detection(frame, holistic)
                 keypoints = extract_keypoints(results)
@@ -89,9 +108,9 @@ def gen():
                     if max_prob_index < len(actions) and res[max_prob_index] > threshold:
                         action = actions[max_prob_index]
                         sentence.append(action)
-                        sentence = sentence[-1:]  # Keep only the latest prediction
+                        sentence = sentence[-1:]
 
-                    image = prob_viz(res, actions, image)
+                    image = prob_viz(res, actions, image, frame_width, frame_height)
 
                 cv2.rectangle(image, (0, 0), (640, 40), (245, 117, 16), -1)
                 cv2.putText(image, ' '.join(sentence), (3, 30),
@@ -116,13 +135,19 @@ def gen():
     cap.release()
 
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    user_agent = request.headers.get('User-Agent').lower()
+    if 'mobi' in user_agent:
+        device_type = "mobile"
+    else:
+        device_type = "pc"
+    return Response(gen(device_type), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/start_camera', methods=['POST'])
 def start_camera():
@@ -137,5 +162,4 @@ def stop_camera():
     return jsonify(success=True)
 
 if __name__ == '__main__':
-    # app.run(debug=True) #esto hace que corra solo en el computador
-    app.run(host='0.0.0.0', port=5000, debug=True) #Esto hace que corra en todos los dispositivos de la casa
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- static/js/layout.js
+++ static/js/layout.js
@@ -1,28 +1,3 @@
-// function changeButton() {
-//     var button = document.getElementById('myButton');
-//     button.classList.remove('btn-success');
-//     button.classList.add('btn-warning');
-//     button.textContent = 'Detener';
-// }
-
-
-
-// function toggleButton() {
-//     var button = document.getElementById('myButton');
-//     if (button.classList.contains('btn-success')) {  
-//     // Para cuando se quiera detener el programa
-//         button.classList.remove('btn-success');
-//         button.classList.add('btn-danger');
-//         button.textContent = 'Detener';
-//     } 
-//     else {
-//     // Para cuando se quiera inicializar el programa
-//         button.classList.remove('btn-danger');
-//         button.classList.add('btn-success');
-//         button.textContent = 'Reproducir';
-//     }
-// }
-
 function toggleButton() {
     var button = document.getElementById('myButton');
     if (button.classList.contains('btn-success')) {
